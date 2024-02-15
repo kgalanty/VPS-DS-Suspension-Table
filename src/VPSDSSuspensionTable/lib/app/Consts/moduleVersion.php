4990d6c28a129bd948d521ba99d0ccea00f7ffd3
kgalanty <dataz.karas@gmail.com>
2024-02-15 22:37:02 +0100

    Disabled deleting upon paid

--- src/VPSDSSuspensionTable/lib/app/Consts/moduleVersion.php
+++ src/VPSDSSuspensionTable/lib/app/Consts/moduleVersion.php
@@ -4,5 +4,5 @@ namespace WHMCS\Module\Addon\VPSDSSuspensionTable\app\Consts;
 
 class moduleVersion
 {
-    const VERSION = '0.0.8';
+    const VERSION = '0.0.9';
 }
--- src/VPSDSSuspensionTable/lib/app/Hooks/InvoicePaid.php
+++ src/VPSDSSuspensionTable/lib/app/Hooks/InvoicePaid.php
@@ -5,19 +5,21 @@ use WHMCS\Module\Addon\VPSDSSuspensionTable\app\Models\TicketsStatus;
 
 return function ($vars) {
 
-	$invoice = Item::where(function (Builder $query) use ($vars) {
-		$query->where("invoiceid", $vars['invoiceid'])->where('type', 'Hosting');
-	})
-		->get();
+	return; 
+	//Disabled deletion upon payment
+	// $invoice = Item::where(function (Builder $query) use ($vars) {
+	// 	$query->where("invoiceid", $vars['invoiceid'])->where('type', 'Hosting');
+	// })
+	// 	->get();
 
-	foreach ($invoice as $item) {
-		if ($item->relid) {
-			$isThereSomethingToDelete = TicketsStatus::serviceToDelete($item->relid)->count();
-			if ($isThereSomethingToDelete > 0) {
-				TicketsStatus::serviceToDelete($item->relid)->update(['deleted_at' => \WHMCS\Carbon::now()->toDateTimeString()]);
+	// foreach ($invoice as $item) {
+	// 	if ($item->relid) {
+	// 		$isThereSomethingToDelete = TicketsStatus::serviceToDelete($item->relid)->count();
+	// 		if ($isThereSomethingToDelete > 0) {
+	// 			TicketsStatus::serviceToDelete($item->relid)->update(['deleted_at' => \WHMCS\Carbon::now()->toDateTimeString()]);
 
-				logActivity('VPSDS marked as deleted upon invoice paid. Invoice id: ' . $vars['invoiceid'] . ', service id: ' . $item->relid);
-			}
-		}
-	}
+	// 			logActivity('VPSDS marked as deleted upon invoice paid. Invoice id: ' . $vars['invoiceid'] . ', service id: ' . $item->relid);
+	// 		}
+	// 	}
+	// }
 };
